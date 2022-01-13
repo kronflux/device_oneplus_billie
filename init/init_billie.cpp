f3600809209bc423d88046b14759b4c32f8221ba
Flux <richard@rmoser.ca>
2022-01-13 14:43:57 -0700

    billie: Support both single sim and multi sim variants

--- init/init_billie.cpp
+++ init/init_billie.cpp
@@ -1,90 +1,60 @@
 /*
-   Copyright (c) 2020, The LineageOS Project
-                 2020, The Potato Open Sauce Project
-
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions are
-   met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
-    * Neither the name of The Linux Foundation nor the names of its
-      contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
-
-   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
-   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
-   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
-   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (C) 2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-#include <android-base/properties.h>
-#include <stdlib.h>
+#include <cstdlib>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
-#include <stdio.h>
 #include <sys/_system_properties.h>
 #include <sys/sysinfo.h>
-#include <sys/system_properties.h>
 
+#include <android-base/properties.h>
 #include "property_service.h"
 #include "vendor_init.h"
 
 using android::base::GetProperty;
+using std::string;
 
-void property_override(char const prop[], char const value[]) {
-  prop_info *pi;
+void property_override(string prop, string value)
+{
+    auto pi = (prop_info*) __system_property_find(prop.c_str());
 
-  pi = (prop_info *)__system_property_find(prop);
-  if (pi)
-    __system_property_update(pi, value, strlen(value));
-  else
-    __system_property_add(prop, strlen(prop), value, strlen(value));
+    if (pi != nullptr)
+        __system_property_update(pi, value.c_str(), value.size());
+    else
+        __system_property_add(prop.c_str(), prop.size(), value.c_str(), value.size());
 }
 
-void load_12gb() {
-  property_override("dalvik.vm.heapstartsize", "24m");
-  property_override("dalvik.vm.heapgrowthlimit", "384m");
-  property_override("dalvik.vm.heaptargetutilization", "0.42");
-  property_override("dalvik.vm.heapmaxfree", "56m");
-}
+void vendor_load_properties()
+{
 
-void load_8gb() {
-  property_override("dalvik.vm.heapstartsize", "24m");
-  property_override("dalvik.vm.heapgrowthlimit", "256m");
-  property_override("dalvik.vm.heaptargetutilization", "0.46");
-  property_override("dalvik.vm.heapmaxfree", "48m");
-}
+    string multiprop;
+    string simpleprop;
+    string model = GetProperty("ro.boot.product.hardware.sku", "");
 
-void load_6gb() {
-  property_override("dalvik.vm.heapstartsize", "16m");
-  property_override("dalvik.vm.heapgrowthlimit", "256m");
-  property_override("dalvik.vm.heaptargetutilization", "0.5");
-  property_override("dalvik.vm.heapmaxfree", "32m");
-}
+    // Check whether model is a single or dual SIM variant and override properties accordingly
+    if ((model == "8" || model == "9")) {
+        // Model is a Single-SIM Variant
+        multiprop = "ssss"; // Set Multi-SIM config to ssss (single-sim)
+        simpleprop = "true"; // Disable connectivity simplification behavior (Android Default)
+    } else {
+        // Model is a Dual-SIM Variant
+        multiprop = "dsds"; // Set Multi-SIM config to dsds (dual-sim)
+        simpleprop = "false"; // Enable connectivity simplification behavior (Allows switching Data between SIM cards)
+    }
 
-/* Check RAM size for current variant. */
-void checkram_loadprops() {
-  struct sysinfo sys;
-  sysinfo(&sys);
-  if (sys.totalram > 8192ull * 1024 * 1024) {
-    load_12gb();
-  } else if (sys.totalram > 6144ull * 1024 * 1024) {
-    load_8gb();
-  } else if (sys.totalram > 4096ull * 1024 * 1024) {
-    load_6gb();
-  }
-}
-
-void vendor_load_properties() {
-  checkram_loadprops();
+    // Set multisim config
+    property_override("persist.radio.multisim.config", multiprop);
+    property_override("persist.sys.fflag.override.settings_provider_model", simpleprop);
 }
